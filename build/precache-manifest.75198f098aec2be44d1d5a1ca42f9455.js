32fef9ec555d2e6213c510f9316ca1b2807ff34e
jairosierra20 <jairosierra204@hotmail.com>
2020-08-22 17:05:26 -0600

    Commit 6 - Crear el Build para la PWA

---
+++ build/precache-manifest.75198f098aec2be44d1d5a1ca42f9455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1bb47becc4ff99f392cdd1cd2bb0bd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53fe61551e031723605d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ba1a6a79393379981533",
+    "url": "/static/js/2.e4a6447b.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/static/js/2.e4a6447b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fe61551e031723605d",
+    "url": "/static/js/main.8a698ce8.chunk.js"
+  },
+  {
+    "revision": "95adf63064254859832d",
+    "url": "/static/js/runtime-main.9096aa25.js"
+  }
+]);
